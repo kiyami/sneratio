2148ad13e1888e193b521fda1074c1e11e994e41
kiyami <kiyami_erdim@hotmail.com>
2021-01-27 22:08:45 +0300

    setup.py and requirements updated, .flaskenv added

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export FLASK_APP=sneratio
+export FLASK_ENV=development
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sneratio",
-    version="0.1.0",
+    version="0.3.0",
     author="M.Kıyami ERDİM",
     author_email="kiyami_erdim@hotmail.com",
 
@@ -22,22 +22,17 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 
     include_package_data=True,
     package_data = {
-        'sneratio': ['data/mass_numbers/*',
-                    'data/solar/*',
-                    'data/test_data/*',
-                    'data/yields/cc/nomoto_2006/*',
-                    'data/yields/cc/nomoto_2013/*',
-                    'data/yields/cc/tsujimoto/*',
-                    'data/yields/Ia/iwamoto/*']
+        'sneratio/src': ['data/*']
     },
 
     install_requires=[
-        "pyside2>=5.13.2",
-        "pandas>=1.1.3",
-        "matplotlib>=3.3.2"
+        "flask>=1.1.2",
+        "pandas>=1.2.1",
+        "matplotlib>=3.3.3",
+        "scipy>=1.6.0",
     ],
 )
